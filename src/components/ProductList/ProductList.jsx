8e5d37ad0dd86c3199ad9f0ce6a01725e4fab8d0
Mihail950 <91260240+Mihail950@users.noreply.github.com>
2023-06-09 06:04:25 +0300

    Update ProductList.jsx

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -5,14 +5,14 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Товар 1', price: 5000, description: 'Описание товара'},
+    {id: '2', title: 'Товар 2', price: 12000, description: 'Описание товара'},
+    {id: '3', title: 'Товар 3', price: 5000, description: 'Описание товара'},
+    {id: '4', title: 'Товар 4', price: 122, description: 'Описание товара'},
+    {id: '5', title: 'Товар 5', price: 5000, description: 'Описание товара'},
+    {id: '6', title: 'Товар 6', price: 600, description: 'Описание товара'},
+    {id: '7', title: 'Товар 7', price: 5500, description: 'Описание товара'},
+    {id: '8', title: 'Товар 8', price: 12000, description: 'Описание товара'},
 ]
 
 const getTotalPrice = (items = []) => {
